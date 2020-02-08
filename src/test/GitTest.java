65426464dd3e0da9e239ed86207a5b4248759b35
javadv2020 <javadv2020@gmail.com>
2020-02-08 15:58:01 +0900

    aaa

--- src/test/GitTest.java
+++ src/test/GitTest.java
@@ -4,7 +4,10 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("asdasd");
 		System.out.println("aaaaa");
+<<<<<<< Updated upstream
 		System.out.println("asdasd");
 		System.out.println("aaaaa");
+=======
+>>>>>>> Stashed changes
 	}
 }
