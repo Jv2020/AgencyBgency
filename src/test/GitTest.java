d8ce4e986d2fe0c1604dd50df6e2e17d932ebbda
Sun ju Kim <ksunju0610@naver.com>
2020-02-08 15:55:35 +0900

    ㄹㄹㄹ

--- src/test/GitTest.java
+++ src/test/GitTest.java
@@ -3,10 +3,8 @@ package test;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("asdasd");
-<<<<<<< HEAD
 		System.out.println("aaaaa");
-=======
-		System.out.println("acnjkcnkdsnk");
->>>>>>> 23f8e42e636c3094668787b952dfd6e69794aa9a
+		System.out.println("asdasd");
+		System.out.println("aaaaa");
 	}
 }
