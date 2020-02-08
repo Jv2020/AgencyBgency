433d9e1037125ff25a5dd8d49cd05ca39cb74226
Sun ju Kim <ksunju0610@naver.com>
2020-02-08 16:01:47 +0900

    ㅎㅎ

--- src/test/GitTest.java
+++ src/test/GitTest.java
@@ -3,15 +3,6 @@ package test;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("asdasd");
-<<<<<<< Updated upstream
-		System.out.println("aaaaa");
-<<<<<<< Updated upstream
-		System.out.println("asdasd");
-		System.out.println("aaaaa");
-=======
->>>>>>> Stashed changes
-=======
-		System.out.println("aaaaa");
->>>>>>> Stashed changes
+		
 	}
 }
