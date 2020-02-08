a9290202ec974bd5d669b20f6ef032d88db145a5
javadv2020 <javadv2020@gmail.com>
2020-02-08 16:03:56 +0900

    asasc

--- src/test/GitTest.java
+++ src/test/GitTest.java
@@ -3,6 +3,10 @@ package test;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("asdasd");
+<<<<<<< Updated upstream
 		
+=======
+		System.out.println("test");
+>>>>>>> Stashed changes
 	}
 }
