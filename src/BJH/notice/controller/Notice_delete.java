bffa996cc7dd69dfdf892543a349b9fed79f1d33
BaeJunHyo <55498777+BaeJunHyo@users.noreply.github.com>
2020-02-10 13:06:48 +0900

    배준효- admin/amain.jsp 제일 하단 Ajax -> sevlert (Notice_delete) 배열 넘기기??

--- src/BJH/notice/controller/Notice_delete.java
+++ src/BJH/notice/controller/Notice_delete.java
@@ -1,7 +1,40 @@
 package BJH.notice.controller;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Notice_delete")
 public class Notice_delete extends HttpServlet {
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("ajax!!!!!!!!!!!!!!!!doget");
+		String[] test = req.getParameterValues("pdeleteList");
+		System.out.println(test);
+		for (int i = 0; i < test.length; i++) {
+			System.out.println(test[i]);
+		}
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("ajax!!!!!!!!!!!!!!!!dopost");
+//		for (int i = 0; i < req.getParameter("deleteList").length(); i++) {
+//			
+//		}
+		String[] test = req.getParameterValues("pdeleteList");
+		System.out.println(test);
+		for (int i = 0; i < test.length; i++) {
+			System.out.println(test[i]);
+		}
+	}
 }
