3a10a2b52c1cac54f17732b72e76296b1919b521
Sun ju Kim <ksunju0610@naver.com>
2020-02-17 09:34:07 +0900

    김선주 : 텍스트에디터 ( 썸머노트 )

---
+++ src/KSJ/exhibit/controller/ExhibitCuratorDetail.java
@@ -0,0 +1,25 @@
+package KSJ.exhibit.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/exhibitcuratordetail")
+public class ExhibitCuratorDetail extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	
+		
+	
+	
+	}
+
+	
+	
+}
